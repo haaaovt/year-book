777eaac988bd2a9c6bac8b38d2162cf46652c802
haaaovt <128685736+haaaovt@users.noreply.github.com>
2024-03-11 00:00:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! :3  Hẹn gặp b ở ngày 17 tới nhaa";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
